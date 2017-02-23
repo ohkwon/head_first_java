c33768c9a4bffa86b63cc0e655cbb69c6b423503
O'Hoon Kwon <ohkwon@ucdavis.edu>
2017-02-22 20:20:45 -0800

    addeed more notes and final info

--- ch10NumbersAndStatics/Song.java
+++ ch10NumbersAndStatics/Song.java
@@ -13,6 +13,7 @@ public class Song {
   public void play() {
     SoundPlayer player = new SoundPlayer();
     // this uses the instance varialbe "title" so it is a non-static variable
+    // static methods can't use non-static methods
     player.playSound(title);
   }
 
@@ -43,5 +44,29 @@ class SoundPlayer {
   // doesn't use an instance variable, but is a side method, not the purpose of the demonstration of this
   public void playSound(String title) {
     System.out.println(title);
+  }
+}
+
+// this class is to demonstrate static variables
+class Duck {
+  private int size;
+
+  // static variable keeps the same value for all classes. it's a shared variable. 
+  private static int duckCount = 0;
+
+  // once a variable (both static and non-static), methods, and class are labeled FINAL, it can never be chantged
+  final static float pi = 3.14159;
+
+  public Duck() {
+    // constructor, each time a new instance of the class is created, the variable duckCount increases by 1
+    duckCount++;
+  }
+
+  public void setSize(int s) {
+    size = s;
+  }
+
+  public int getSize() {
+    return size;
   }
 }
